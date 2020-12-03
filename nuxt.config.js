ef33db32d16622ffdf71f1f68693742959ca2a3e
Poom <poompongphun@gmail.com>
2020-12-03 21:05:50 +0700

    add server port

--- nuxt.config.js
+++ nuxt.config.js
@@ -62,4 +62,8 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {},
+  server: {
+    port: 8000, // default: 3000
+    host: '0.0.0.0', // default: localhost
+  },
 }
