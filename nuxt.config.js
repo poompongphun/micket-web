7345f01b89ef0e1e00ec46712220d19fd5201cc6
Poom <poompongphun@gmail.com>
2020-12-06 00:39:12 +0700

    update viewport

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,11 @@ export default {
     title: 'Micket',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        name: 'viewport',
+        content:
+          'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0',
+      },
       { hid: 'description', name: 'description', content: '' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
