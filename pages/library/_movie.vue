6388bd572fa01b4ed3822aafd3a7c7371f3a7ddb
Poom <poompongphun@gmail.com>
2021-01-13 00:18:48 +0700

    update library

---
+++ pages/library/_movie.vue
@@ -0,0 +1,103 @@
+<template>
+  <div>
+    <v-expand-transition>
+      <v-card
+        v-show="video !== ''"
+        class="iconBg elevation-0"
+        width="100%"
+        height="390"
+      >
+        <v-responsive :key="video" class="black mx-auto" width="700">
+          <vue-plyr :options="options" style="width: 100%">
+            <video previewThumbnails controls playsinline>
+              <source size="480" :src="video" type="video/mp4" />
+              <source size="720" :src="video" type="video/mp4" />
+              <source size="1080" :src="video" type="video/mp4" />
+            </video>
+          </vue-plyr>
+        </v-responsive>
+      </v-card>
+    </v-expand-transition>
+    <v-tabs
+      v-model="tab"
+      background-color="defaultBg"
+      show-arrows
+      mobile-breakpoint="0"
+    >
+      <v-tab v-for="(season, i) in movies" :key="i">
+        {{ season.name }}
+      </v-tab>
+    </v-tabs>
+
+    <v-tabs-items v-model="tab" class="pt-3 defaultBg">
+      <v-tab-item v-for="(season, i) in movies" :key="i">
+        <v-card class="defaultBg" flat>
+          <v-row dense>
+            <v-col
+              v-for="movie in season.movie"
+              :key="movie._id"
+              cols="12"
+              sm="6"
+              md="4"
+              lg="3"
+              xl="2"
+            >
+              <v-card
+                class="elevation-0 defaultBg"
+                :disabled="movie.media.video === video"
+                @click="playVdo(movie.media.video)"
+              >
+                <v-card>
+                  <v-img :src="movie.media.thumbnail"></v-img>
+                </v-card>
+                <h3
+                  class="py-1 overflow-hidden"
+                  style="height: 60px; font-size: 1.1rem"
+                >
+                  {{ movie.name }}
+                </h3>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-tab-item>
+    </v-tabs-items>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $axios, params, store }) {
+    const ownedMovie = await $axios.$get(
+      `/api/users/me/library/${params.movie}`,
+      {
+        progress: false,
+      }
+    )
+    return {
+      movies: ownedMovie,
+    }
+  },
+  data: () => ({
+    tab: null,
+    renderComponent: true,
+    options: {
+      ratio: '16:9',
+      autoplay: true,
+      //   controls: ['play', 'progress', 'current-time'],
+      //   fullscreen: {
+      //     enabled: false,
+      //     fallback: true,
+      //     iosNative: false,
+      //     container: null,
+      //   },
+    },
+    video: '',
+  }),
+  methods: {
+    playVdo(url) {
+      this.video = url
+    },
+  },
+}
+</script>
---
+++ pages/library/index.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <div v-if="movies.length !== 0">
+      <h1>Library</h1>
+      <v-row dense align="center">
+        <v-col
+          v-for="movie in movies"
+          :key="movie._id"
+          cols="6"
+          sm="4"
+          md="3"
+          lg="2"
+        >
+          <v-card @click="$router.push(`/library/${movie._id}`)">
+            <v-img :src="movie.poster.y"></v-img>
+          </v-card>
+        </v-col>
+      </v-row>
+    </div>
+    <div v-else class="center-screen text-center">
+      <v-img
+        class="mx-auto"
+        :src="require('~/assets/img/online_video.svg')"
+        contain
+        :width="
+          $vuetify.breakpoint.xs
+            ? '90%'
+            : $vuetify.breakpoint.mdAndDown
+            ? '70%'
+            : '50%'
+        "
+      >
+        <div class="d-flex center-in-img">
+          <div class="no-movie navbar">
+            <h2>Not found your movie :(</h2>
+            <v-btn class="my-3" color="primary" depressed to="/">
+              Buy one
+            </v-btn>
+          </div>
+        </div>
+      </v-img>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $axios }) {
+    const responseMovie = await $axios.$get(`/api/users/me/library/`, {
+      progress: false,
+    })
+    return { movies: responseMovie.library }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.center-in-img {
+  align-items: center;
+  bottom: 0;
+  justify-content: center;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  .no-movie {
+    opacity: 0.9;
+    width: 100%;
+    border-radius: 30px;
+    padding: 15px;
+  }
+}
+</style>
