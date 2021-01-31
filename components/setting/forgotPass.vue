a897a90b683e3d05fc39b482e3f9099f9b8ba021
Poom <poompongphun@gmail.com>
2021-01-31 18:42:00 +0700

    add forgotpass validate

--- components/setting/forgotPass.vue
+++ components/setting/forgotPass.vue
@@ -22,7 +22,7 @@
               <div class="py-3">
                 <v-text-field
                   v-model="email"
-                  :error-messages="mailError"
+                  :error-messages="mailError === '' ? emailErrors : mailError"
                   class="my-2"
                   name="email"
                   label="Email"
@@ -32,13 +32,20 @@
                   single-line
                   hint="Please enter your email to find your account."
                   rounded
+                  @keypress="mailError = ''"
+                  @input="$v.email.$touch()"
+                  @blur="$v.email.$touch()"
                 ></v-text-field>
               </div>
             </v-card>
 
             <div class="float-right">
-              <v-btn text @click="dialog = false"> Cancel </v-btn>
-              <v-btn color="primary" @click="sendMail({ email })">
+              <v-btn text @click="success()"> Cancel </v-btn>
+              <v-btn
+                color="primary"
+                :disabled="emailErrors.length !== 0 || email === ''"
+                @click="sendMail({ email })"
+              >
                 Continue
               </v-btn>
             </div>
@@ -46,7 +53,10 @@
 
           <v-stepper-content step="2">
             <v-card class="elevation-0" height="140">
-              <h3 class="pb-1">Check code in your mail</h3>
+              <h3 class="pb-1">
+                Check code in your mail
+                <v-chip small>{{ email }}</v-chip>
+              </h3>
               <v-divider></v-divider>
               <div class="py-3">
                 <v-text-field
@@ -61,13 +71,18 @@
                   single-line
                   hint="Please check the code in your email. Your code is a 6-digit number."
                   rounded
+                  @keypress="isCode($event, code)"
                 ></v-text-field>
               </div>
             </v-card>
 
             <div class="float-right">
-              <v-btn text @click="step = 1"> Cancel </v-btn>
-              <v-btn color="primary" @click="checkCode({ email, code })">
+              <v-btn text @click="success()"> Cancel </v-btn>
+              <v-btn
+                color="primary"
+                :disabled="code === ''"
+                @click="checkCode({ email, code })"
+              >
                 Continue
               </v-btn>
             </div>
@@ -80,6 +95,7 @@
               <div class="py-3">
                 <v-text-field
                   v-model="newPassword"
+                  :error-messages="passwordErrors"
                   class="my-2"
                   name="password"
                   label="New Password"
@@ -89,14 +105,17 @@
                   hint="Choose a new password"
                   single-line
                   rounded
+                  @input="$v.newPassword.$touch()"
+                  @blur="$v.newPassword.$touch()"
                 ></v-text-field>
               </div>
             </v-card>
 
             <div class="float-right">
-              <v-btn text @click="step = 2"> Cancel </v-btn>
+              <v-btn text @click="success()"> Cancel </v-btn>
               <v-btn
                 color="primary"
+                :disabled="passwordErrors.length !== 0"
                 @click="changePassword({ email, code, newPassword })"
               >
                 Continue
@@ -132,7 +151,23 @@
 </template>
 
 <script>
+import { validationMixin } from 'vuelidate'
+import { required, minLength, maxLength, email } from 'vuelidate/lib/validators'
+
 export default {
+  mixins: [validationMixin],
+
+  validations: {
+    email: {
+      required,
+      email,
+    },
+    newPassword: {
+      required,
+      minLength: minLength(6),
+      maxLength: maxLength(128),
+    },
+  },
   data: () => ({
     dialog: false,
     step: 1,
@@ -143,6 +178,25 @@ export default {
     mailError: '',
     codeError: '',
   }),
+  computed: {
+    emailErrors() {
+      const errors = []
+      if (!this.$v.email.$dirty) return errors
+      !this.$v.email.email && errors.push('Must be valid e-mail')
+      !this.$v.email.required && errors.push('E-mail is required')
+      return errors
+    },
+    passwordErrors() {
+      const errors = []
+      if (!this.$v.newPassword.$dirty) return errors
+      !this.$v.newPassword.minLength &&
+        errors.push('Password must be at least 6 characters long')
+      !this.$v.newPassword.maxLength &&
+        errors.push('Password must be at most 128 characters long')
+      !this.$v.newPassword.required && errors.push('Password is required')
+      return errors
+    },
+  },
   methods: {
     open() {
       this.dialog = true
@@ -178,6 +232,7 @@ export default {
         }
       } catch (error) {
         this.codeError = 'Wrong code'
+        this.code = ''
       }
     },
     async changePassword(value) {
@@ -202,6 +257,15 @@ export default {
         })
       }
     },
+    isCode($event, code) {
+      const keyCode = $event.keyCode ? $event.keyCode : $event.which
+      if (
+        ((keyCode < 48 || keyCode > 57) && keyCode !== 46) ||
+        code.length === 6
+      ) {
+        $event.preventDefault()
+      }
+    },
     success() {
       this.dialog = false
       this.step = 1
@@ -210,6 +274,7 @@ export default {
       this.newPassword = ''
       this.mailError = ''
       this.codeError = ''
+      this.$v.$reset()
     },
   },
 }
